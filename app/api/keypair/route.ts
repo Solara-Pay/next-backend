306709553937bafbe288cda2b28f0fad1b506acb
Don-Vicks <shallangwavictorzakka@gmail.com>
2024-09-22 16:07:20 +0100

    Keypair Slight Modification

--- app/api/keypair/route.ts
+++ app/api/keypair/route.ts
@@ -1,17 +1,21 @@
 import { Keypair } from "@solana/web3.js";
 import { NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 export async function GET() {
   const { publicKey, secretKey } = Keypair.generate();
 
   console.log(`The public key is: ${publicKey.toBase58()}`);
   console.log(`The secret key is: ${secretKey}`);
 
-  const response = NextResponse.json({
+  return NextResponse.json({
     publicKey: publicKey.toBase58(),
     secretKey: Array.from(secretKey), // Convert Uint8Array to a regular array for JSON compatibility
+  }, {
+    headers: {
+      'Cache-Control': 'no-store, max-age=0, must-revalidate',
+    },
   });
-
-  response.headers.set('Cache-Control', 'no-store');
-  return response;
 }
